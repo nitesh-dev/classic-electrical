cf3e271b1f6f43c2b171186fe90ea24d5af9c108
nitesh-dev <niteshdev547@gmail.com>
2023-09-29 23:22:04 +0530

    getintouch section done

---
+++ components/GetInTouch.vue
@@ -0,0 +1,139 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <section class="touch">
+        <div class="page">
+            <h2>Get in touch</h2>
+            <p>With the Internet spreading linke wildfire and reaching every part of our daily life, more and more traffic
+                is directed to websites in search for information.</p>
+            <div class="form-holder">
+                <form>
+                    <input type="text" placeholder="Enter Your Name">
+                    <input type="text" placeholder="Enter Your Email">
+                    <input type="text" placeholder="Subject">
+                    <input type="text" placeholder="Message">
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <WidgetsArrowTextButton><span>Contact Now</span></WidgetsArrowTextButton>
+
+                </form>
+                <div class="right">
+                    <div>
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 23 23" fill="none">
+                                <path
+                                    d="M11.5 0.71875C6.94312 0.71875 3.23438 4.4275 3.23438 8.98438C3.23438 16.1216 10.3716 21.7566 10.6734 21.9938C10.9178 22.1878 11.2053 22.2813 11.5 22.2813C11.7947 22.2813 12.0822 22.1878 12.3266 21.9938C12.6284 21.7566 19.7656 16.1216 19.7656 8.98438C19.7656 4.4275 16.0569 0.71875 11.5 0.71875ZM11.5 12.19C9.63125 12.19 8.11469 10.6734 8.11469 8.80469C8.11469 6.93594 9.63125 5.41938 11.5 5.41938C13.3687 5.41938 14.8853 6.93594 14.8853 8.80469C14.8853 10.6734 13.3687 12.19 11.5 12.19Z"
+                                    fill="white" />
+                            </svg>
+                        </div>
+                        <div>
+                            <h4>Address</h4>
+                            <p>10-11,Gr. Floor, Ambedakar Shopping Center, Mandarwaja, Ring Road, Surat, Gujarat-395002</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path
+                                    d="M22.0448 17.52C21.3892 16.9703 17.5448 14.5358 16.9058 14.6475C16.6058 14.7008 16.3763 14.9565 15.762 15.6893C15.4779 16.0499 15.166 16.3877 14.829 16.6995C14.2116 16.5504 13.614 16.3288 13.0485 16.0395C10.831 14.9599 9.03947 13.1679 7.9605 10.95C7.67118 10.3846 7.44964 9.78691 7.3005 9.1695C7.6123 8.83252 7.95009 8.52056 8.31075 8.2365C9.04275 7.62225 9.29925 7.39425 9.3525 7.09275C9.46425 6.45225 7.0275 2.60925 6.48 1.95375C6.2505 1.68225 6.042 1.5 5.775 1.5C5.001 1.5 1.5 5.829 1.5 6.39C1.5 6.43575 1.575 10.9425 7.26675 16.7333C13.0575 22.425 17.5642 22.5 17.61 22.5C18.171 22.5 22.5 18.999 22.5 18.225C22.5 17.958 22.3177 17.7495 22.0448 17.52ZM17.25 11.25H18.75C18.7482 9.65925 18.1155 8.13416 16.9907 7.00933C15.8658 5.8845 14.3408 5.25179 12.75 5.25V6.75C13.9431 6.75119 15.087 7.22568 15.9307 8.06933C16.7743 8.91299 17.2488 10.0569 17.25 11.25Z"
+                                    fill="white" />
+                                <path
+                                    d="M21 11.25H22.5C22.497 8.66505 21.4688 6.18683 19.641 4.359C17.8132 2.53116 15.3349 1.50298 12.75 1.5V3C14.9372 3.00258 17.0342 3.8726 18.5808 5.41922C20.1274 6.96584 20.9974 9.06276 21 11.25Z"
+                                    fill="white" />
+                            </svg>
+                        </div>
+                        <div>
+                            <h4>Phone</h4>
+                            <p>+91-9825117510 (Naresh K. Sarasiya)</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                    d="M2.94576 4.61067L9.42951 11.0972C10.8436 12.5086 13.1552 12.5099 14.5705 11.0972L21.0543 4.61067C21.0696 4.59543 21.0814 4.577 21.0888 4.5567C21.0962 4.53639 21.0991 4.5147 21.0972 4.49316C21.0953 4.47163 21.0887 4.45078 21.0778 4.43209C21.0669 4.4134 21.0521 4.39733 21.0343 4.38502C20.373 3.92381 19.5684 3.65039 18.7017 3.65039H5.29842C4.43165 3.65039 3.62704 3.92386 2.96573 4.38502C2.94796 4.39733 2.93312 4.4134 2.92225 4.43209C2.91139 4.45078 2.90477 4.47163 2.90286 4.49316C2.90096 4.5147 2.90381 4.53639 2.91123 4.5567C2.91864 4.577 2.93043 4.59543 2.94576 4.61067ZM1.21308 7.73569C1.21255 7.07661 1.37272 6.42735 1.67972 5.84414C1.69043 5.82358 1.70585 5.80584 1.72471 5.79236C1.74358 5.77888 1.76536 5.77004 1.78828 5.76656C1.8112 5.76308 1.83463 5.76505 1.85664 5.77233C1.87866 5.7796 1.89864 5.79197 1.91498 5.80842L8.31665 12.2101C10.3439 14.2402 13.655 14.2414 15.6834 12.2101L22.0851 5.80842C22.1014 5.79197 22.1214 5.7796 22.1434 5.77233C22.1655 5.76505 22.1889 5.76308 22.2118 5.76656C22.2347 5.77004 22.2565 5.77888 22.2754 5.79236C22.2942 5.80584 22.3097 5.82358 22.3204 5.84414C22.6273 6.42736 22.7875 7.07662 22.787 7.73569V16.264C22.787 18.5183 20.9532 20.3493 18.7017 20.3493H5.29842C3.04692 20.3493 1.21308 18.5183 1.21308 16.264V7.73569Z"
+                                    fill="white" />
+                            </svg>
+                        </div>
+                        <div>
+                            <h4>Email</h4>
+                            <p>inquiry@classicelectricals.in
+                                nksabb@yahoo.co.in
+                                Klassic_elec@yahoo.com</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped>
+.touch h2 {
+    margin: 0;
+    text-align: center;
+}
+
+.touch .page {
+    padding: 80px 70px;
+    background-color: var(--color-surface-variant);
+}
+
+.touch .page>p {
+    color: var(--color-on-surface-700);
+    text-align: center;
+    max-width: 850px;
+    margin: auto;
+}
+
+.touch input {
+    padding: 20px;
+    display: block;
+    background-color: transparent;
+    width: 100%;
+    outline: none;
+    border: none;
+    margin-bottom: 24px;
+    border-radius: 5px;
+    border: 1px solid #232323;
+}
+
+
+.touch .form-holder {
+    margin-top: 66px;
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    gap: 60px;
+}
+
+
+.touch .right>div {
+    display: grid;
+    grid-template-columns: max-content 1fr;
+    gap: 20px;
+}
+
+
+.touch .icon {
+    width: 40px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid white;
+    border-radius: 50%;
+}
+
+.touch h4 {
+    margin: 0;
+}
+
+.touch .right p {
+    color: var(--color-on-surface-700);
+    margin-top: 0.5em;
+}
+</style>
