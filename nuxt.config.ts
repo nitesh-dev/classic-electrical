d95b0d1f23f051aba087dcb384e9d097dea4ab3f
nitesh-dev <niteshdev547@gmail.com>
2023-09-29 12:27:37 +0530

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['nuxt-swiper'],
+  swiper: {
+    // Swiper options
+    //----------------------
+    // prefix: 'Swiper',
+    // styleLang: 'css',
+    // modules: ['navigation', 'pagination'], // all modules are imported by default
+  },
+  devtools: { enabled: false },
+  css: ["@/assets/styles/global.css", "@/assets/styles/form.css"],
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Outfit",
+        },
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css?family=Work Sans",
+        },
+      ],
+    },
+  },
+  ssr: false,
+  nitro: {
+    preset: "service-worker",
+  },
+});
