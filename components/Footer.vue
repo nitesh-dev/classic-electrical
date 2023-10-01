3ac8b0a4d5d142aab7c4a1e42982db52b90978d1
nitesh-dev <niteshdev547@gmail.com>
2023-10-01 12:50:14 +0530

    footer done

---
+++ components/Footer.vue
@@ -0,0 +1,305 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <footer>
+        <div>
+            <div class="page">
+                <div class="top">
+                    <img src="../public/images/logo.png" alt="logo">
+                    <h2>How we can help you? <br>
+                        Let's talk</h2>
+                </div>
+
+                <div class="bottom">
+                    <div class="left">
+                        <p>Subscribe to our newsletter</p>
+                        <div class="input-box">
+                            <input type="text" placeholder="Email Address">
+                            <div class="button">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"
+                                    fill="none">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M17.9591 9.35874C18.3124 9.71199 18.3124 10.2885 17.9591 10.6418L11.0399 17.5608C10.6861 17.9146 10.1106 17.9146 9.75686 17.5608L9.36983 17.1737C9.01573 16.8196 9.01565 16.2444 9.36979 15.8903L14.6186 10.6419C14.9728 10.2878 14.9728 9.71253 14.6186 9.35843L9.36983 4.11007C9.01565 3.75593 9.01569 3.18078 9.36987 2.82664L9.7569 2.4396C10.1101 2.08636 10.6867 2.08636 11.0399 2.4396L17.9591 9.35874ZM3.71139 2.43953C3.3578 2.08589 2.78174 2.08593 2.42815 2.43953L2.04112 2.82656C1.68768 3.17999 1.68764 3.75656 2.04112 4.11003L7.28967 9.35843C7.64381 9.71257 7.64381 10.2878 7.28967 10.6419L2.04108 15.8903C1.68748 16.2439 1.68752 16.8202 2.04108 17.1738L2.42811 17.5608C2.78186 17.9146 3.3576 17.9146 3.71135 17.5608L10.016 11.256C11.7845 13.0228 11.3371 12.5747 10.0173 11.2547L10.6301 10.6418C10.9836 10.2883 10.9836 9.71218 10.6301 9.3587L3.71139 2.43953Z"
+                                        fill="url(#paint0_linear_47_226)" />
+                                    <defs>
+                                        <linearGradient id="paint0_linear_47_226" x1="26.1403" y1="7.89496" x2="-9.29826"
+                                            y2="7.89496" gradientUnits="userSpaceOnUse">
+                                            <stop stop-color="#161616" />
+                                            <stop offset="0.739533" stop-color="#161616" stop-opacity="0.12" />
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="right">
+
+                        <div class="content">
+                            <div class="links">
+                                <span>Quick link</span>
+                                <div>
+                                    <NuxtLink to="/">About Us</NuxtLink>
+                                    <NuxtLink to="/">Services</NuxtLink>
+                                    <NuxtLink to="/">Products</NuxtLink>
+                                    <NuxtLink to="/">Reviews</NuxtLink>
+                                    <NuxtLink to="/">Clients</NuxtLink>
+                                    <NuxtLink to="/">Contact Us</NuxtLink>
+                                </div>
+                            </div>
+
+                            <div class="contact">
+                                <span>Contact Us</span>
+                                <div class="address">
+
+                                    <div>
+                                        <div class="icon">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                viewBox="0 0 24 24" fill="none">
+                                                <path
+                                                    d="M22.0448 17.52C21.3892 16.9703 17.5448 14.5358 16.9058 14.6475C16.6058 14.7008 16.3763 14.9565 15.762 15.6893C15.4779 16.0499 15.166 16.3877 14.829 16.6995C14.2116 16.5504 13.614 16.3288 13.0485 16.0395C10.831 14.9599 9.03947 13.1679 7.9605 10.95C7.67118 10.3846 7.44964 9.78691 7.3005 9.1695C7.6123 8.83252 7.95009 8.52056 8.31075 8.2365C9.04275 7.62225 9.29925 7.39425 9.3525 7.09275C9.46425 6.45225 7.0275 2.60925 6.48 1.95375C6.2505 1.68225 6.042 1.5 5.775 1.5C5.001 1.5 1.5 5.829 1.5 6.39C1.5 6.43575 1.575 10.9425 7.26675 16.7333C13.0575 22.425 17.5642 22.5 17.61 22.5C18.171 22.5 22.5 18.999 22.5 18.225C22.5 17.958 22.3177 17.7495 22.0448 17.52ZM17.25 11.25H18.75C18.7482 9.65925 18.1155 8.13416 16.9907 7.00933C15.8658 5.8845 14.3408 5.25179 12.75 5.25V6.75C13.9431 6.75119 15.087 7.22568 15.9307 8.06933C16.7743 8.91299 17.2488 10.0569 17.25 11.25Z"
+                                                    fill="white" />
+                                                <path
+                                                    d="M21 11.25H22.5C22.497 8.66505 21.4688 6.18683 19.641 4.359C17.8132 2.53116 15.3349 1.50298 12.75 1.5V3C14.9372 3.00258 17.0342 3.8726 18.5808 5.41922C20.1274 6.96584 20.9974 9.06276 21 11.25Z"
+                                                    fill="white" />
+                                            </svg>
+                                        </div>
+                                        <p>+91-9825117510 (Naresh K. Sarasiya)</p>
+                                    </div>
+
+                                    <div>
+                                        <div class="icon">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                viewBox="0 0 24 24" fill="none">
+                                                <path fill-rule="evenodd" clip-rule="evenodd"
+                                                    d="M2.94576 4.61067L9.42951 11.0972C10.8436 12.5086 13.1552 12.5099 14.5705 11.0972L21.0543 4.61067C21.0696 4.59543 21.0814 4.577 21.0888 4.5567C21.0962 4.53639 21.0991 4.5147 21.0972 4.49316C21.0953 4.47163 21.0887 4.45078 21.0778 4.43209C21.0669 4.4134 21.0521 4.39733 21.0343 4.38502C20.373 3.92381 19.5684 3.65039 18.7017 3.65039H5.29842C4.43165 3.65039 3.62704 3.92386 2.96573 4.38502C2.94796 4.39733 2.93312 4.4134 2.92225 4.43209C2.91139 4.45078 2.90477 4.47163 2.90286 4.49316C2.90096 4.5147 2.90381 4.53639 2.91123 4.5567C2.91864 4.577 2.93043 4.59543 2.94576 4.61067ZM1.21308 7.73569C1.21255 7.07661 1.37272 6.42735 1.67972 5.84414C1.69043 5.82358 1.70585 5.80584 1.72471 5.79236C1.74358 5.77888 1.76536 5.77004 1.78828 5.76656C1.8112 5.76308 1.83463 5.76505 1.85664 5.77233C1.87866 5.7796 1.89864 5.79197 1.91498 5.80842L8.31665 12.2101C10.3439 14.2402 13.655 14.2414 15.6834 12.2101L22.0851 5.80842C22.1014 5.79197 22.1214 5.7796 22.1434 5.77233C22.1655 5.76505 22.1889 5.76308 22.2118 5.76656C22.2347 5.77004 22.2565 5.77888 22.2754 5.79236C22.2942 5.80584 22.3097 5.82358 22.3204 5.84414C22.6273 6.42736 22.7875 7.07662 22.787 7.73569V16.264C22.787 18.5183 20.9532 20.3493 18.7017 20.3493H5.29842C3.04692 20.3493 1.21308 18.5183 1.21308 16.264V7.73569Z"
+                                                    fill="white" />
+                                            </svg>
+                                        </div>
+                                        <p>inquiry@classicelectricals.in
+                                            nksabb@yahoo.co.in
+                                            Klassic_elec@yahoo.com</p>
+                                    </div>
+
+                                    <div>
+                                        <div class="icon">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                                viewBox="0 0 23 23" fill="none">
+                                                <path
+                                                    d="M11.5 0.71875C6.94312 0.71875 3.23438 4.4275 3.23438 8.98438C3.23438 16.1216 10.3716 21.7566 10.6734 21.9938C10.9178 22.1878 11.2053 22.2813 11.5 22.2813C11.7947 22.2813 12.0822 22.1878 12.3266 21.9938C12.6284 21.7566 19.7656 16.1216 19.7656 8.98438C19.7656 4.4275 16.0569 0.71875 11.5 0.71875ZM11.5 12.19C9.63125 12.19 8.11469 10.6734 8.11469 8.80469C8.11469 6.93594 9.63125 5.41938 11.5 5.41938C13.3687 5.41938 14.8853 6.93594 14.8853 8.80469C14.8853 10.6734 13.3687 12.19 11.5 12.19Z"
+                                                    fill="white" />
+                                            </svg>
+                                        </div>
+                                        <p>10-11,Gr. Floor, Ambedakar Shopping Center, Mandarwaja, Ring Road, Surat,
+                                            Gujarat-395002</p>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="copyright">
+                    <p>© 2023 Classic Electricals. Built using WordPress and the Materialis Theme</p>
+                    <div class="socials">
+
+                        <!-- insta -->
+                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                                <g clip-path="url(#clip0_47_278)">
+                                    <path
+                                        d="M14.2323 3.33057C13.9914 3.33057 13.7953 3.52663 13.7953 3.76756C13.7953 4.00848 13.9914 4.20455 14.2323 4.20455C14.4733 4.20455 14.6693 4.00852 14.6693 3.76756C14.6693 3.5266 14.4733 3.33057 14.2323 3.33057ZM8.99999 5.12582C6.86383 5.12582 5.12592 6.86373 5.12592 8.9999C5.12592 11.1361 6.86383 12.874 8.99999 12.874C11.1362 12.874 12.8741 11.1361 12.8741 8.99993C12.8741 6.86377 11.1362 5.12582 8.99999 5.12582Z"
+                                        fill="white" />
+                                    <path
+                                        d="M13.0656 0H4.93443C2.21358 0 0 2.21358 0 4.93446V13.0656C0 15.7865 2.21358 18 4.93443 18H13.0656C15.7865 18 18 15.7864 18 13.0656V4.93446C18 2.21358 15.7865 0 13.0656 0ZM9 13.9371C6.27768 13.9371 4.06297 11.7223 4.06297 9C4.06297 6.27768 6.27771 4.06301 9 4.06301C11.7223 4.06301 13.9371 6.27771 13.9371 9C13.9371 11.7223 11.7223 13.9371 9 13.9371ZM14.2323 5.26767C13.4053 5.26767 12.7324 4.59478 12.7324 3.7677C12.7324 2.94061 13.4053 2.26768 14.2323 2.26768C15.0594 2.26768 15.7323 2.94057 15.7323 3.76766C15.7323 4.59475 15.0594 5.26767 14.2323 5.26767Z"
+                                        fill="white" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_47_278">
+                                        <rect width="18" height="18" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </a>
+
+                        <!-- facebook -->
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+                                <path
+                                    d="M8.888 12.1442H6.71C6.358 12.1442 6.248 12.0122 6.248 11.6822V9.02017C6.248 8.66817 6.38 8.55817 6.71 8.55817H8.888V6.62217C8.888 5.74217 9.042 4.90617 9.482 4.13617C9.944 3.34417 10.604 2.81617 11.44 2.50817C11.99 2.31017 12.54 2.22217 13.134 2.22217H15.29C15.598 2.22217 15.73 2.35417 15.73 2.66217V5.17017C15.73 5.47817 15.598 5.61017 15.29 5.61017C14.696 5.61017 14.102 5.61017 13.508 5.63217C12.914 5.63217 12.606 5.91817 12.606 6.53417C12.584 7.19417 12.606 7.83217 12.606 8.51417H15.158C15.51 8.51417 15.642 8.64617 15.642 8.99817V11.6602C15.642 12.0122 15.532 12.1222 15.158 12.1222H12.606V19.2942C12.606 19.6682 12.496 19.8002 12.1 19.8002H9.35C9.02 19.8002 8.888 19.6682 8.888 19.3382V12.1442Z"
+                                    fill="white" />
+                            </svg>
+                        </a>
+
+                        <!-- twitter -->
+                        <a href="#">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                                <g clip-path="url(#clip0_47_291)">
+                                    <path
+                                        d="M18 3.41887C17.3306 3.7125 16.6174 3.90712 15.8737 4.00162C16.6388 3.54487 17.2226 2.82712 17.4971 1.962C16.7839 2.38725 15.9964 2.68763 15.1571 2.85525C14.4799 2.13413 13.5146 1.6875 12.4616 1.6875C10.4186 1.6875 8.77387 3.34575 8.77387 5.37863C8.77387 5.67113 8.79862 5.95237 8.85938 6.22012C5.7915 6.0705 3.07687 4.60013 1.25325 2.36025C0.934875 2.91263 0.748125 3.54488 0.748125 4.2255C0.748125 5.5035 1.40625 6.63637 2.38725 7.29225C1.79437 7.281 1.21275 7.10888 0.72 6.83775V6.87825C0.72 8.6715 1.99912 10.161 3.6765 10.5041C3.37612 10.5863 3.04875 10.6256 2.709 10.6256C2.47275 10.6256 2.23425 10.6121 2.01038 10.5626C2.4885 12.024 3.84525 13.0984 5.4585 13.1332C4.203 14.1154 2.60888 14.7071 0.883125 14.7071C0.5805 14.7071 0.29025 14.6936 0 14.6565C1.63462 15.7106 3.57188 16.3125 5.661 16.3125C12.4515 16.3125 16.164 10.6875 16.164 5.81175C16.164 5.64862 16.1584 5.49113 16.1505 5.33475C16.8829 4.815 17.4982 4.16587 18 3.41887Z"
+                                        fill="white" />
+                                </g>
+                                <defs>
+                                    <clipPath id="clip0_47_291">
+                                        <rect width="18" height="18" fill="white" />
+                                    </clipPath>
+                                </defs>
+                            </svg>
+                        </a>
+
+                        <!-- telegram -->
+                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+                                <path
+                                    d="M1.79919 10.8843L6.22669 12.3878L16.7393 5.9611C16.8919 5.86829 17.0479 6.07522 16.9166 6.19622L8.95813 13.5215L8.6625 17.6225C8.65767 17.6912 8.67384 17.7597 8.70886 17.819C8.74388 17.8783 8.7961 17.9256 8.85859 17.9545C8.92109 17.9834 8.9909 17.9927 9.05877 17.981C9.12664 17.9694 9.18937 17.9374 9.23863 17.8892L11.6889 15.4795L16.1686 18.8703C16.6513 19.236 17.3504 18.9782 17.4804 18.387L20.5996 4.22447C20.7776 3.41666 19.9856 2.73466 19.2136 3.03029L1.77719 9.70729C1.22994 9.91698 1.24506 10.6959 1.79919 10.8843Z"
+                                    fill="white" />
+                            </svg></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+</template>
+<style scoped>
+footer {
+    background-color: var(--color-surface);
+    overflow: auto;
+}
+
+footer>div {
+    margin-top: 170px;
+    background-color: var(--color-surface-variant);
+}
+
+footer .top,
+footer .bottom {
+    display: grid;
+    grid-template-columns: 1fr 1.5fr;
+    gap: 200px;
+}
+
+footer .top {
+    padding-top: 90px;
+    padding-bottom: 40px;
+    align-items: center;
+}
+
+footer h2 {
+    font-size: calc(var(--big-font) - 5px);
+    margin: 0;
+    font-family: 'Heebo' !important;
+    font-weight: 500;
+}
+
+footer .input-box {
+    display: grid;
+    grid-template-columns: 1fr max-content;
+    align-items: center;
+    border-bottom: 1px solid #505050;
+}
+
+footer .left .button {
+    display: flex;
+    width: 43px;
+    height: 43px;
+    align-items: center;
+    justify-content: center;
+    background-color: white;
+    border-radius: 50%;
+}
+
+footer .left p {
+    margin-top: 50px;
+    margin-bottom: 15px;
+}
+
+footer .left input {
+    padding: 20px;
+    border: none;
+    outline: none;
+    background-color: transparent;
+    width: 100%;
+}
+
+footer .content {
+    display: grid;
+    grid-template-columns: 1fr 1.5fr;
+    gap: 120px;
+}
+
+footer .links div {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 14px;
+    margin-top: 30px;
+}
+
+footer .links a {
+    color: var(--color-on-surface-700);
+}
+
+
+
+footer .address {
+    margin-top: 30px;
+}
+
+footer .address>div {
+    display: grid;
+    grid-template-columns: max-content 1fr;
+    gap: 20px;
+}
+
+
+footer .address .icon {
+    width: 34px;
+    height: 34px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid white;
+    border-radius: 50%;
+}
+
+footer .address p {
+    color: var(--color-on-surface-700);
+    margin-top: 0;
+}
+
+
+
+
+
+/* copy right */
+
+footer .copyright{
+    padding: 20px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-top: 1px solid #272727;
+    margin-top: 50px;
+}
+footer .copyright p{
+    color: var(--color-on-surface-700);
+}
+
+footer .copyright .socials{
+    display: flex;
+    align-items: center;
+    gap: 15px;
+}
+
+footer .copyright .socials a{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 36px;
+    height: 36px;
+    border: 1px solid var(--color-on-surface);
+    border-radius: 50%;
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -149,6 +149,7 @@ const aboutImages = [about1, about2, about3, about4, about5, about6, about7, abo
     <GetInTouch/>
 
     <FAQ/>
+    <Footer></Footer>
 </template>
 
 
