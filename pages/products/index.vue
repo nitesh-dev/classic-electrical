02fee6587c3bb562c85ad019e8f5b0b75a597508
nitesh-dev <niteshdev547@gmail.com>
2023-10-03 14:01:00 +0530

    products page done

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -12,71 +12,71 @@
                         <h4>Electrical Control Panel</h4>
                         <p>Required for fully automated plant and also to control the power factor near to unity as well as
                             for machine controlling and research & development purpose.</p>
-                        <NuxtLink>
+                        <NuxtLink to="/products/readmore/Transformer">
                             View More
-                            <img src="../public/images/arrow.png">
+                            <img src="../../public/images/arrow.png">
                         </NuxtLink>
                     </div>
 
                     <div class="content">
                         <h4>Transformer</h4>
                         <p>To transfer and distribute the power for different circuits as well as to reduce the losses in power transmission.</p>
-                        <NuxtLink>
+                        <NuxtLink to="/products/readmore/Transformer">
                             View More
-                            <img src="../public/images/arrow.png">
+                            <img src="../../public/images/arrow.png">
                         </NuxtLink>
                     </div>
 
                     <div class="content">
                         <h4>Load Break Switch (LBS)</h4>
                         <p>Updated version of GOAB. And having compact in size and to break the transformer inlet power from main incomer and also provide protection in case of faulty condition with emergency indication.</p>
-                        <NuxtLink>
+                        <NuxtLink to="/products/readmore/Transformer">
                             View More
-                            <img src="../public/images/arrow.png">
+                            <img src="../../public/images/arrow.png">
                         </NuxtLink>
                     </div>
 
                     <div class="content">
                         <h4>Gang Operated Air-break Switch</h4>
                         <p>used to break the transformer inlet power from main incomer and also provide protection in case of faulty condition</p>
-                        <NuxtLink>
+                        <NuxtLink to="/products/readmore/Transformer">
                             View More
-                            <img src="../public/images/arrow.png">
+                            <img src="../../public/images/arrow.png">
                         </NuxtLink>
                     </div>
 
                     <div class="content">
                         <h4>All types of Earthing</h4>
                         <p>Earthing is used to protect the human being from leakage current in the machines or any conductive places.</p>
-                        <NuxtLink>
+                        <NuxtLink to="/products/readmore/Transformer">
                             View More
-                            <img src="../public/images/arrow.png">
+                            <img src="../../public/images/arrow.png">
                         </NuxtLink>
                     </div>
 
                     <div class="content">
                         <h4>Power Cables</h4>
                         <p>A power cable is an electrical cable, an assembly of one or more electrical conductors, usually held together with an overall sheath. The assembly is used for transmission of electrical power.</p>
-                        <NuxtLink>
+                        <NuxtLink to="/products/readmore/Transformer">
                             View More
-                            <img src="../public/images/arrow.png">
+                            <img src="../../public/images/arrow.png">
                         </NuxtLink>
                     </div>
 
                     <div class="content">
                         <h4>Vaccum Circuit Breakers</h4>
                         <p>Provide the breaking in case of faulty condition and also reduce the spark in case of humidity.</p>
-                        <NuxtLink>
+                        <NuxtLink to="/products/readmore/Transformer">
                             View More
-                            <img src="../public/images/arrow.png">
+                            <img src="../../public/images/arrow.png">
                         </NuxtLink>
                     </div>
 
                 </div>
                 <div class="right">
-                    <img class="round" src="../public/images/products/Rectangle 1209.jpg">
-                    <img class="round" src="../public/images/products/Rectangle 1210.jpg">
-                    <img class="round" src="../public/images/products/Rectangle 1211.jpg">
+                    <img class="round" src="../../public/images/products/Rectangle 1209.jpg">
+                    <img class="round" src="../../public/images/products/Rectangle 1210.jpg">
+                    <img class="round" src="../../public/images/products/Rectangle 1211.jpg">
                 </div>
             </div>
         </div>
---
+++ pages/products/readmore/[title].vue
@@ -0,0 +1,89 @@
+<script setup lang='ts'>
+</script>
+<template>
+    <OtherLanding title="Products" />
+
+    <section class="products">
+        <div class="page">
+            <h2>Electrical Control Panel</h2>
+            <p>We Value Our Clients and Hope for Longlastling relationship with them.Here are our Some of Clients</p>
+            <div class="info">
+                <img class="round" src="../../../public/images/products/Rectangle 1209.jpg">
+                <div>
+                    <h4>What's in an Electrical Control Panel?</h4>
+                    <p>A control panel is a box that contains electrical components required to run a factory machine or
+                        process. A typical industrial control panel component list includes:</p>
+                    <div class="list">
+                        <p>Breakers, suppressors, & fuses</p>
+                        <p>Power distribution</p>
+                        <p>Power supply</p>
+                        <p>Communications</p>
+                        <p>Motor drives</p>
+                        <p>Terminal blocks</p>
+                        <p>Relays & contactors</p>
+                        <p>HMI</p>
+                        <p>Transformer</p>
+                        <p>Enclosure, backplate, & grounding panels</p>
+                        <p>Wire duct & DIN rail</p>
+                    </div>
+                </div>
+            </div>
+
+            <p>The control panel also goes by the name of PLC panel, with PLC short for programmable logic
+                        controller. This means the PLC is basically an industrial-grade computer, and that high voltage and
+                        high stakes are at play with many of these components.</p>
+                    <p>No two applications and environments are identical, but here's what you can expect to see in an
+                        electrical control panel.</p>
+
+        </div>
+    </section>
+
+    <Footer></Footer>
+</template>
+<style scoped>
+
+.products h2{
+    margin-top: 0;
+    margin-bottom: 0;
+}
+
+.products p{
+    color: var(--color-on-surface-700);
+}
+
+
+.products .info {
+    margin-top: 50px;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 50px;
+    margin-bottom: 50px;
+}
+
+
+.products h4{
+    margin: 0;
+}
+
+.products img{
+    width: 100%;
+}
+
+
+.products .list p{
+    margin: 8px 0;
+    display: flex;
+    gap: 10px;
+}
+
+.products .list p::before{
+    content: url('../../../public/images/arrow-gray.png');
+    margin-top: 4px;
+}
+
+.products .info>div>p{
+    color: var(--color-on-surface);
+
+}
+
+</style>
